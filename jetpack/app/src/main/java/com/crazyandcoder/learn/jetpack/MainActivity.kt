6011d41d849b26891e25d77d1e8fd5101a2e2e71
liji <lijiwork@sina.com>
2021-07-19 10:23:07 +0800

    add navigation demo

--- jetpack/app/src/main/java/com/crazyandcoder/learn/jetpack/MainActivity.kt
+++ jetpack/app/src/main/java/com/crazyandcoder/learn/jetpack/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.crazyandcoder.learn.jetpack
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.crazyandcoder.learn.jetpack.navigation.BottomNavigationActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //navigation
+        navigationTv.setOnClickListener {
+            startActivity(Intent(this@MainActivity, BottomNavigationActivity::class.java))
+        }
+
     }
 }
